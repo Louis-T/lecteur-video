8ac67bb473137ff40a359bc7633bc7d2fa81a40a
Louis-T <louis.tribouillard@gmail.com>
2015-12-23 13:33:12 +0100

    fixed untracked files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// main.js
-var React = require('react');
-var ReactDOM = require('react-dom');
-var VideoPlayer = require('./components/VideoPlayer.js');
-
-ReactDOM.render(
-	<VideoPlayer />,
-	document.getElementById('container')
-);
