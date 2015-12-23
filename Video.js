199d7f36c955a166e59ef1c250cd85919b64dbae
Louis-T <louis.tribouillard@gmail.com>
2015-12-23 09:49:19 +0100

    Initial Commit

---
+++ Video.js
@@ -0,0 +1,81 @@
+var React = require('react');
+var ProgressBar = require('./ProgressBar.js');
+
+var Video = React.createClass({
+	getInitialState: function(){
+	    return {
+	       duration: 0,
+		   currentTime: 0,
+		   paused: true
+	    };
+	},
+	getDefaultProps: function(){
+		return {
+			width: 640,
+			height: 360
+		};
+	},
+	getPausedText: function(){
+		return this.state.paused?"Play":"Pause";
+	},
+	componentDidMount: function() {
+		console.log(this.refs.Video.duration);
+	},
+	componentDidUpdate: function (props, state) {
+		if (this.state.paused && !state.paused) {
+			this.pauseVideo();
+		} else if (!this.state.paused && state.paused) {
+			this.playVideo();
+		}
+	},
+	onLoadedMetadata: function() {
+		console.log(this.refs.Video.duration);
+		//this.refs.Video.currentTime = 4.2456;
+		this.setState({
+			duration: this.refs.Video.duration
+		});
+	},
+	onTimeUpdate: function() {
+		this.setState({
+			currentTime: this.refs.Video.currentTime
+		});
+	},
+	setVideoTime: function(t) {
+		if(t < 0){
+			
+		}
+		else if(t > this.state.duration){
+			
+		}
+		else {
+			this.refs.Video.currentTime = t;
+		}
+	},
+	togglePlay: function(){
+		this.setState({
+			paused: !this.state.paused
+		});
+	},
+	pauseVideo: function(){
+		this.refs.Video.pause();
+	},
+	playVideo: function(){
+		if(!this.state.paused){ // pour pallier au probleme du curseur que l'on bouge pour choisir le temps
+			this.refs.Video.play();
+		}
+	},
+	render: function(){
+		return (
+			<div>
+				<video className="video" width={this.props.width} height={this.props.height} controls ref="Video" onLoadedMetadata={this.onLoadedMetadata} onTimeUpdate={this.onTimeUpdate}>
+					<source src="./ressources/big_buck_bunny.mp4" type="video/mp4" />
+					Your browser does not support the video tag.
+				</video>
+				<button onClick={this.togglePlay} >{this.getPausedText()}</button>
+				<ProgressBar width={this.props.width} videoDuration={this.state.duration} currentTime={this.state.currentTime} setVideoTime={this.setVideoTime} pauseVideo={this.pauseVideo} playVideo={this.playVideo} />
+			</div>
+		);
+	}
+});
+
+module.exports = Video;
