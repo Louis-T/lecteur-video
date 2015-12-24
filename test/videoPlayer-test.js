7cc35d03ea88ab0dd8087d70c0a3a8c70937480b
Louis-T <louis.tribouillard@gmail.com>
2015-12-24 11:31:25 +0100

    tests

--- test/videoPlayer-test.js
+++ test/videoPlayer-test.js
@@ -6,19 +6,52 @@ var assert = require('assert');
 var React = require('react');
 var TestUtils = require('react-addons-test-utils');
 
+var VideoPlayer = require('../js/components/VideoPlayer.js');
+var Video = require('../js/components/Video.js');
+var ProgressBar = require('../js/components/ProgressBar.js');
+
 describe('Testing my VideoPlayer', function() {
   jsdom({ skipWindowCheck: true });
 
-  it('test', function() {
-    var VideoPlayer = require('../js/components/VideoPlayer.js');
+  it('should switch button classes', function() {
     var myVideoPlayer = TestUtils.renderIntoDocument(
       <VideoPlayer />
     );
-    var video = TestUtils.findRenderedDOMComponentWithTag(myVideoPlayer, 'video');
-	var btn = TestUtils.findRenderedDOMComponentWithTag(myVideoPlayer, 'button');
+    var video_dom = TestUtils.findRenderedDOMComponentWithTag(myVideoPlayer, 'video');	
+	var video_react = TestUtils.findRenderedComponentWithType(myVideoPlayer, Video);
+	var btn_dom = TestUtils.findRenderedDOMComponentWithClass(video_react, 'videoBtn');
 
-	assert.equal(btn.textContent, "Play");
-	TestUtils.Simulate.click(btn);
-	assert.equal(btn.textContent, "Pause");
+	assert.equal(btn_dom.className, "videoBtn videoBtnPaused");
+	assert.equal(video_react.state.paused, true);
+	TestUtils.Simulate.click(btn_dom);
+	assert.equal(btn_dom.className, "videoBtn");
+	assert.equal(video_react.state.paused, false);
   });
+
+});
+
+describe('Testing my VideoPlayer async', function() {
+  jsdom({ skipWindowCheck: true });
+	var myVideoPlayer = TestUtils.renderIntoDocument(
+		<VideoPlayer />
+	);
+	var video_react = TestUtils.findRenderedComponentWithType(myVideoPlayer, Video);
+	var progressBar_react = TestUtils.findRenderedComponentWithType(video_react, ProgressBar);
+    var btn_dom = TestUtils.findRenderedDOMComponentWithClass(myVideoPlayer, 'videoBtn');
+	TestUtils.Simulate.click(btn_dom);
+	
+	  beforeEach(function(done){  
+		setTimeout(function(){
+			
+		  // complete the async beforeEach
+		  done();
+
+		}, 500);
+
+	  });
+
+	  it("will pass", function(){
+		  console.log(progressBar_react.state, video_react.state, myVideoPlayer.state); 
+		assert.equal(true, true);
+	  });
 });
